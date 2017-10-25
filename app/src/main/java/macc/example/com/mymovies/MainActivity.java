edea84816af290032e81d5e7c89e143d4c1c2b69
RBeraldi <beraldi@dis.uniroma1.it>
2017-10-25 09:39:18 +0200

    asynchronous loaad

--- app/src/main/java/macc/example/com/mymovies/MainActivity.java
+++ app/src/main/java/macc/example/com/mymovies/MainActivity.java
@@ -1,5 +1,6 @@
 package macc.example.com.mymovies;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -9,21 +10,26 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MovieAdapter movieAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        movieAdapter = new MovieAdapter();
         if (savedInstanceState==null){
             Log.i("INFO","Initializing Data Source...");
-            MovieDataSource.initialize();
+            MovieDataSource.initializeAsync(movieAdapter,this);
+            //MovieDataSource.initialize();
             Log.i("INFO","Data Source Initialized...");
         }
-
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.progress);
         RecyclerView rv = findViewById(R.id.rv);
-        rv.setAdapter(new MovieAdapter());
+        //rv.setAdapter(new MovieAdapter());
+        rv.setAdapter(movieAdapter);
         LinearLayoutManager lm = new LinearLayoutManager(this);
         //GridLayoutManager gm = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
-        lm.setOrientation(LinearLayoutManager.HORIZONTAL);
+       // lm.setOrientation(LinearLayoutManager.HORIZONTAL);
         rv.setLayoutManager(lm);
+
     }
 }
--- app/src/main/java/macc/example/com/mymovies/MovieDataSource.java
+++ app/src/main/java/macc/example/com/mymovies/MovieDataSource.java
@@ -1,5 +1,9 @@
 package macc.example.com.mymovies;
 
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+
 import java.util.ArrayList;
 
 
@@ -14,6 +18,28 @@ public class MovieDataSource {
             Movie movie = new Movie("Il signore degli anelli","Bel film","2000",i);//String title, String description, String releaseDate, int rating
             movies.add(movie);
         }
+    }
+    static public void initializeAsync(final MovieAdapter movieAdapter, final Activity activity){
+
+        movies.clear();
+        new Thread((new Runnable(){
+            @Override
+            public void run() {
+                for(int i=0;i<100;i++){
+                    for (int j=0;j<10000000;j++);
+                    Movie movie = new Movie("Il Signore degli Anelli",""+i,"2000",i);//String title, String description, String releaseDate, int rating
+                    movies.add(movie);
+                    Log.i("INFO",""+i);
+                }
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        activity.findViewById(R.id.progressBar).setVisibility(View.GONE);
+                        movieAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        })).start();
     }
     static public int size(){
         return movies.size();
